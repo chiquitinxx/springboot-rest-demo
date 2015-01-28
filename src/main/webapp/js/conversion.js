7db78b65ef4284938c50096dc5bdffa74ede68c6
Jorge Franco <jorge.franco.leza@gmail.com>
2015-01-28 23:57:32 +0100

    Upgrade to 1.0.0

--- src/main/webapp/js/conversion.js
+++ src/main/webapp/js/conversion.js
@@ -1,5 +1,7 @@
 require(['./common'], function () {
-    require(['jquery', 'ace', 'app/ConvertedCode', 'app/ConversionPresenter'], function($) {
-        ConversionPresenter().init('/conversion', '#buttonConvert', '#clearButton');
+    require(['jquery', 'grooscript.min', 'grooscript-tools','ace'], function($) {
+        require(['app/ConvertedCode', 'app/ConversionPresenter'], function() {
+            ConversionPresenter().init('/conversion', '#buttonConvert', '#clearButton');
+        });
     });
 });
