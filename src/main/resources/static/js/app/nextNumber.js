394a76f6e1a5852d69db4ed1cae897330058dbcf
Jorge Franco <jorge.franco.leza@gmail.com>
2015-05-28 22:28:47 +0200

    Cleanup

--- src/main/resources/static/js/app/nextNumber.js
+++
@@ -1,4 +0,0 @@
-//Grooscript converted file
-var nextNumber = function(number) {
-  return gs.mc(gs.mc(this,"$",["body"]),"html",[gs.execStatic(Templates,'applyTemplate', this,["counterBody.tpl", gs.map().add("number",gs.plus(number, 1))])]);
-};
